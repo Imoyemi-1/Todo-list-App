2d890bc913ac194df7528375e2fd3e1192721d83
Imoyemi <okeibrahim133@gmail.com>
2025-06-11 08:57:58 -0500

    updating css file

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 // Global variables
-const todoInput = document.getElementById('input-container');
+const todoInput = document.getElementById("input-container");
 
 // Add new todo to dom
 
 const addNewTodo = (e) => {
   e.preventDefault();
-  let getItemFromStorage = JSON.parse(localStorage.getItem('todo')) || [];
+  let getItemFromStorage = JSON.parse(localStorage.getItem("todo")) || [];
 
-  const input = document.getElementById('addtodoitem');
+  const input = document.getElementById("addtodoitem");
   const randomId = crypto.randomUUID().slice(-8);
 
   const todoItem = getItemFromStorage.map((item) => item.txt);
 
-  if (input.value.trim() === '') {
-    alert('Please add todo item');
+  if (input.value.trim() === "") {
+    alert("Please add todo item");
   } else if (todoItem.indexOf(input.value.trim()) !== -1) {
-    alert('Todo already exist in the list');
+    alert("Todo already exist in the list");
   } else {
     createTodo(input.value.trim(), false, randomId);
     if (!getItemFromStorage) {
@@ -28,14 +28,14 @@ const addNewTodo = (e) => {
     });
   }
 
-  localStorage.setItem('todo', JSON.stringify(getItemFromStorage));
+  localStorage.setItem("todo", JSON.stringify(getItemFromStorage));
 
-  input.value = '';
+  input.value = "";
 };
 // display item to dom
 
 const displayTodo = () => {
-  const getItemFromStorage = JSON.parse(localStorage.getItem('todo'));
+  const getItemFromStorage = JSON.parse(localStorage.getItem("todo"));
 
   if (getItemFromStorage) {
     getItemFromStorage.forEach((item) =>
@@ -45,21 +45,21 @@ const displayTodo = () => {
 };
 // Create todo element
 function createTodo(text, isActive, id) {
-  const todoContainer = document.getElementById('todo-Item-container');
+  const todoContainer = document.getElementById("todo-Item-container");
 
   // create list element
-  const li = document.createElement('li');
-  li.classList = 'todo-item';
-  li.setAttribute('data-id', id);
+  const li = document.createElement("li");
+  li.classList = "todo-item";
+  li.setAttribute("data-id", id);
   li.innerHTML = `<div class="todo-item-txt-container">
               <input
                 type="checkbox"
                 name="checktodo"
                 class = "checktodo"
-                ${isActive ? 'checked' : 'unchecked'}
+                ${isActive ? "checked" : "unchecked"}
               />
               <p class="${
-                !isActive ? 'todo-txt' : 'todo-txt checkcomplete'
+                !isActive ? "todo-txt" : "todo-txt checkcomplete"
               }">${capitalizeFirstWord(text)}</p>
             </div>
             <button class="remove-todo-btn">
@@ -71,28 +71,28 @@ function createTodo(text, isActive, id) {
   todoContainer.appendChild(li);
 
   document
-    .querySelectorAll('.todo-item-txt-container')
-    .forEach((item) => item.addEventListener('click', completeTodo));
+    .querySelectorAll(".todo-item-txt-container")
+    .forEach((item) => item.addEventListener("click", completeTodo));
   document
-    .querySelectorAll('.remove-todo-btn')
-    .forEach((item) => item.addEventListener('click', removeTodo));
+    .querySelectorAll(".remove-todo-btn")
+    .forEach((item) => item.addEventListener("click", removeTodo));
 }
 
 // Toggle todo check complete
 
 const completeTodo = (e) => {
-  const todoId = e.currentTarget.parentElement.getAttribute('data-id');
+  const todoId = e.currentTarget.parentElement.getAttribute("data-id");
 
-  if (e.target.classList.contains('checktodo')) {
+  if (e.target.classList.contains("checktodo")) {
     e.target.checked
-      ? e.target.nextElementSibling.classList.add('checkcomplete')
-      : e.target.nextElementSibling.classList.remove('checkcomplete');
+      ? e.target.nextElementSibling.classList.add("checkcomplete")
+      : e.target.nextElementSibling.classList.remove("checkcomplete");
   } else {
     e.target.previousElementSibling.checked =
       !e.target.previousElementSibling.checked;
     e.target.previousElementSibling.checked
-      ? e.target.classList.add('checkcomplete')
-      : e.target.classList.remove('checkcomplete');
+      ? e.target.classList.add("checkcomplete")
+      : e.target.classList.remove("checkcomplete");
   }
 
   toggleActiveStateInStorage(todoId);
@@ -101,7 +101,7 @@ const completeTodo = (e) => {
 // check and toggle active state for todo
 
 function toggleActiveStateInStorage(todoid) {
-  const getItemFromStorage = JSON.parse(localStorage.getItem('todo'));
+  const getItemFromStorage = JSON.parse(localStorage.getItem("todo"));
 
   const updatedTodo = getItemFromStorage.map((todos) => {
     if (todos.id === todoid) {
@@ -112,31 +112,35 @@ function toggleActiveStateInStorage(todoid) {
     }
     return todos;
   });
-  localStorage.setItem('todo', JSON.stringify(updatedTodo));
+  localStorage.setItem("todo", JSON.stringify(updatedTodo));
 }
 
 // RemoveTodo from dom
 
 const removeTodo = (e) => {
-  const getItemFromStorage = JSON.parse(localStorage.getItem('todo'));
+  const getItemFromStorage = JSON.parse(localStorage.getItem("todo"));
   const li = e.target.parentElement.parentElement;
 
   li.remove();
   // remove from storage
   const removedtodo = getItemFromStorage.filter(
-    (todo) => todo.id !== li.getAttribute('data-id')
+    (todo) => todo.id !== li.getAttribute("data-id")
   );
-  localStorage.setItem('todo', JSON.stringify(removedtodo));
+  localStorage.setItem("todo", JSON.stringify(removedtodo));
 };
 
 // remove all completed todo from storage
 function clearCompletedFromStorage() {
-  const getItemFromStorage = JSON.parse(localStorage.getItem('todo'));
+  const getItemFromStorage = JSON.parse(localStorage.getItem("todo"));
 
   // remove complete from storage
   const removeComplete = getItemFromStorage.filter((todo) => !todo.isActive);
-  localStorage.setItem('todo', JSON.stringify(removeComplete));
+  localStorage.setItem("todo", JSON.stringify(removeComplete));
 }
+
+// toggle between todo states btn to filter active states
+
+const filterTodoStates = () => {};
 
 // Utility functions
 function capitalizeFirstWord(word) {
@@ -145,5 +149,5 @@ function capitalizeFirstWord(word) {
 
 // Eventlistener
 
-todoInput.addEventListener('submit', addNewTodo);
+todoInput.addEventListener("submit", addNewTodo);
 displayTodo();
